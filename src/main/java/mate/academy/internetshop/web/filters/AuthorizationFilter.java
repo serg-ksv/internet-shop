0217c198639f9882f27c8e2a7d9fc8971669b9fa
serg-ksv <serg.ksv1@gmail.com>
2020-05-06 17:11:19 +0300

    Added solution

--- src/main/java/mate/academy/internetshop/web/filters/AuthorizationFilter.java
+++ src/main/java/mate/academy/internetshop/web/filters/AuthorizationFilter.java
@@ -16,8 +16,10 @@ import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.model.Role;
 import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.service.UserService;
+import org.apache.log4j.Logger;
 
 public class AuthorizationFilter implements Filter {
+    private static final Logger LOGGER = Logger.getLogger(AuthorizationFilter.class);
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private final UserService userService =
             (UserService) INJECTOR.getInstance(UserService.class);
@@ -59,6 +61,7 @@ public class AuthorizationFilter implements Filter {
         if (isAuthorized(user, protectedUrls.get(url))) {
             chain.doFilter(req, resp);
         } else {
+            LOGGER.info("The user with id=" + userId + " was trying to access a restricted page.");
             req.getRequestDispatcher("/WEB-INF/views/accessDenied.jsp").forward(req, resp);
         }
     }
